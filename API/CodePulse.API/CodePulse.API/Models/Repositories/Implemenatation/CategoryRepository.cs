2ce6e2753226363bf64c4624c8c6ce16dfaf1bdc
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-12-01 15:01:24 +0530

    Adding CodePuls project

---
+++ API/CodePulse.API/CodePulse.API/Models/Repositories/Implemenatation/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Models.Repositories.Implemenatation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<Category> CreateAsync(Category category)
+        {
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
---
+++ API/CodePulse.API/CodePulse.API/Models/Repositories/Interface/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using CodePulse.API.Models.Domain;
+
+namespace CodePulse.API.Models.Repositories.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> CreateAsync(Category category);
+    }
+}
