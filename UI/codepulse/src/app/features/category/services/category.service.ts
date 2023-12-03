90e6d62b9b2f10d469d01f6e6942dbb6c182a58d
Md Azhar Ashraf <azhar.ashraf@milliman.com>
2023-12-03 11:56:41 +0530

    create ui service method to get all category

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request-model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class CategoryService {
 
   addcategory(model: AddCategoryRequest):Observable<void>{
     return this.http.post<void>('https://localhost:7034/api/categories',model);
+  }
+
+  getallcategory():Observable<Category[]>{
+    return this.http.get<Category[]>('https://localhost:7034/api/categories');
   }
 }
